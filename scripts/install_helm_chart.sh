d1c3b89df3d770279e33012f4260dc6af8820f29
Learn DevOps & Cloud <revnaweji@gmail.com>
2025-08-05 12:34:17 -0500

    Update install_helm_chart.sh

--- scripts/install_helm_chart.sh
+++ scripts/install_helm_chart.sh
@@ -2,8 +2,8 @@
 CLUSTER_NAME=${1:-emmanuel-cluster}
 NAMESPACE=${2:-default}
 RELEASE_NAME="emmanuel-services-infra-crossplane"
-echo "🚀 Installing Helm chart '$RELEASE_NAME' to cluster: $CLUSTER_NAME in namespace: $NAMESPACE"
+echo "Installing Helm chart '$RELEASE_NAME' to cluster: $CLUSTER_NAME in namespace: $NAMESPACE"
 helm upgrade --install "$RELEASE_NAME" ./helm \
   --namespace "$NAMESPACE" \
   --create-namespace
-echo "✅ Helm chart deployed to $CLUSTER_NAME"
+echo "Helm chart deployed to $CLUSTER_NAME"
