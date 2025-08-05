bcda44bce80a1fef9d5d624e7fa2c0bcb3c7ec74
Learn DevOps & Cloud <revnaweji@gmail.com>
2025-08-05 12:34:03 -0500

    Update install_crossplane.sh

--- scripts/install_crossplane.sh
+++ scripts/install_crossplane.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "🚀 Installing Crossplane..."
+echo "Installing Crossplane..."
 kubectl create namespace crossplane-system --dry-run=client -o yaml | kubectl apply -f -
 helm repo add crossplane-stable https://charts.crossplane.io/stable
 helm repo update
 helm install crossplane crossplane-stable/crossplane -n crossplane-system
-echo "✅ Crossplane installed in 'crossplane-system' namespace."
+echo "Crossplane installed in 'crossplane-system' namespace."
