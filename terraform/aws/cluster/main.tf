cbc82f733be84d3b35b1f6de67370b937ee8a7f8
Learn DevOps & Cloud <revnaweji@gmail.com>
2025-08-05 13:19:10 -0500

    Add files via upload

---
+++ terraform/aws/cluster/main.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+  region = var.region
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "emmanuel-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["${var.region}a", "${var.region}b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  enable_dns_hostnames = true
+}
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "~> 20.0"
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+  subnet_ids      = module.vpc.public_subnets
+  vpc_id          = module.vpc.vpc_id
+
+  eks_managed_node_groups = {
+    default = {
+      desired_capacity = 2
+      max_capacity     = 3
+      min_capacity     = 1
+      instance_types   = ["t3.medium"]
+    }
+  }
+}
