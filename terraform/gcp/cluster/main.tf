f3306ef4f65499e72b499eec2b25b196249accc8
Learn DevOps & Cloud <revnaweji@gmail.com>
2025-08-05 13:21:17 -0500

    Add files via upload

---
+++ terraform/gcp/cluster/main.tf
@@ -0,0 +1,32 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_container_cluster" "gke" {
+  name     = var.cluster_name
+  location = var.region
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  node_config {
+    machine_type = "e2-medium"
+  }
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "primary-node-pool"
+  cluster    = google_container_cluster.gke.name
+  location   = var.region
+
+  node_count = 2
+
+  node_config {
+    preemptible  = false
+    machine_type = "e2-medium"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+  }
+}
