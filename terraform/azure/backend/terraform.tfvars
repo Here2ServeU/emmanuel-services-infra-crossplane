dff0288f52bad0a7e5d491a317f569edca073ae4
Learn DevOps & Cloud <revnaweji@gmail.com>
2025-08-05 13:14:10 -0500

    Add files via upload

---
+++ terraform/azure/backend/terraform.tfvars
@@ -0,0 +1,3 @@
+resource_group       = "emmanuel-tf-rg"
+location             = "East US"
+storage_account_name = "emmanueltfstate"
---
+++ terraform/azure/backend/variables.tf
@@ -0,0 +1,13 @@
+variable "resource_group" {
+  type    = string
+  default = "emmanuel-tf-rg"
+}
+
+variable "location" {
+  type    = string
+  default = "East US"
+}
+
+variable "storage_account_name" {
+  type = string
+}
