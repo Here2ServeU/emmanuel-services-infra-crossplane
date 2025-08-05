1878387c07bf260ab2b0e3f54b72b16e5bd5383a
Learn DevOps & Cloud <revnaweji@gmail.com>
2025-08-05 12:33:50 -0500

    Update deploy_argocd.sh

--- scripts/deploy_argocd.sh
+++ scripts/deploy_argocd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "📦 Installing ArgoCD..."
+echo "Installing ArgoCD..."
 kubectl create namespace argocd --dry-run=client -o yaml | kubectl apply -f -
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-echo "✅ ArgoCD installed. Use 'kubectl port-forward svc/argocd-server -n argocd 8080:443' to access the UI."
+echo "ArgoCD installed. Use 'kubectl port-forward svc/argocd-server -n argocd 8080:443' to access the UI."
